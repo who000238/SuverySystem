c36de37401570f2d7c3a3091a9b26856860c4193
who000238 <who000238@gmail.com>
2021-10-21 23:03:06 +0800

    統計頁面能夠撈到資料 接下來設計格式讓輸出結果是合乎規定的要求

--- Statistic.aspx
+++ Statistic.aspx
@@ -4,14 +4,30 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title></title>
+    <link rel="stylesheet" href="css/bootstrap.css" />
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="Scripts/jQuery-min-3.6.0.js"></script>
+    <style>
+        div {
+            border: 1px solid
+        }
+    </style>
+
 </head>
 <body>
     <form id="form1" runat="server">
-        <h1>前台</h1>
-        <div>
-            統計頁面
+        <div class="container">
+            <h1>前台</h1>
+            <div class="col-12">
+                統計頁面
+            </div>
+            <div class="col-12">
+                <h3 runat="server" id="h3Title"></h3>
+            </div>
+            <div runat="server" id="TestArea"></div>
         </div>
     </form>
 </body>
--- Statistic.aspx.cs
+++ Statistic.aspx.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using DBSource;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +14,45 @@ namespace SuverySystem
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string StringGuid = Request.QueryString["ID"];
+            Guid guid = Guid.Parse(StringGuid);
+            var dt = GetSuveryAnswerData(guid);
+            GridView gridView = new GridView();
+            gridView.DataSource = dt;
+            gridView.DataBind();
+            this.TestArea.Controls.Add(gridView);
+        }
 
+        public static DataTable GetSuveryAnswerData(Guid guid)
+        {
+            {
+                string connectionString = DBHelper.GetConnectionString();
+                string dbCommandString =
+                                             @" 
+                        SELECT
+	                        [dbo].[SuveryList].[Title],
+	                        [dbo].[SuveryList].[Guid],
+	                        [dbo].[SuveryData].[TypeOrder],
+	                        [dbo].[SuveryData].[NameOrder],
+	                        [dbo].[SuveryAnswer].[Answer]
+                        FROM [dbo].[SuveryList]
+                        JOIN[dbo].[SuveryData] ON[dbo].[SuveryList].[Guid] =  [dbo].[SuveryData].[Guid]
+                        JOIN 	[dbo].[SuveryAnswer] ON [dbo].[SuveryList].[Guid] =	[dbo].[SuveryAnswer].[Guid]
+                        WHERE[dbo].[SuveryList].[Guid]  = @Guid
+                    
+                ";
+                List<SqlParameter> list = new List<SqlParameter>();
+                list.Add(new SqlParameter("@Guid", guid));
+                try
+                {
+                    return DBHelper.ReadDataTable(connectionString, dbCommandString, list);
+                }
+                catch (Exception ex)
+                {
+                    Logger.WriteLog(ex);
+                    return null;
+                }
+            }
         }
     }
 }
