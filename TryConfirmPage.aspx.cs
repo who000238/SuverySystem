adf527483e8d8336a10d09df5c2201adb4f0aba2
who000238 <who000238@gmail.com>
2021-10-29 14:01:34 +0800

    資料庫新增使用者資訊資料表 問卷回答資料表新增使用者資訊欄位 用於後台觀看細節，填寫資料時關聯鍵，建置後台問卷查看填寫資料頁程式碼

--- TryConfirmPage.aspx.cs
+++ TryConfirmPage.aspx.cs
@@ -20,20 +20,28 @@ namespace SuverySystem
             }
             string StringGuid = Request.QueryString["ID"];
             Guid guid = Guid.Parse(StringGuid);
-
-            string ansString = this.Session["ansString"].ToString(); //Session內存放的回答字串
+            //Session內存放的回答字串
+            string ansString = this.Session["ansString"].ToString(); 
             string[] ansStringArray = ansString.Split(',');
-            
-            string SuveryStatus;
-            var SuveryMasterDataRow = GetSuveryMasterData(guid); //取得問卷基本資料
-            var QuestionDetailDT = GetQuestionDetailAndItemDetail(guid); //取得問題資料
 
-            if (SuveryMasterDataRow!=null)
+            string UserInfoString = this.Session["UserInfo"].ToString();
+            string[] UserInfoArray = UserInfoString.Split(',');
+            Label lblForUserInfo = new Label();
+            lblForUserInfo.Text = $"姓名 : {UserInfoArray[0]} </br> 電話 : {UserInfoArray[1]}</br>E-Mail : {UserInfoArray[2]}</br> 年齡 : {UserInfoArray[3]} </br></br>";
+            this.AnsArea.Controls.Add(lblForUserInfo);
+
+            string SuveryStatus;
+            //取得問卷基本資料
+            var SuveryMasterDataRow = GetSuveryMasterData(guid);
+            //取得問題資料
+            var QuestionDetailDT = GetQuestionDetailAndItemDetail(guid); 
+
+            if (SuveryMasterDataRow != null)
             {
-            if (SuveryMasterDataRow["Status"].ToString() == "N")
-                SuveryStatus = "關閉中";
-            else
-                SuveryStatus = "開放中";
+                if (SuveryMasterDataRow["Status"].ToString() == "N")
+                    SuveryStatus = "關閉中";
+                else
+                    SuveryStatus = "開放中";
                 this.ltlStatusAndDate.Text = $"{SuveryStatus}</br>{SuveryMasterDataRow["StartDate"]}~{SuveryMasterDataRow["EndDate"]}";
                 this.h3Title.InnerText = SuveryMasterDataRow["Title"].ToString();
                 for (int i = 0; i < QuestionDetailDT.Rows.Count; i++)
@@ -50,7 +58,12 @@ namespace SuverySystem
         {
             string StringGuid = Request.QueryString["ID"];
             Guid guid = Guid.Parse(StringGuid);
-            var QuestionDetailDT = GetQuestionDetailAndItemDetail(guid); //取得問題資料
+            //取得問題資料
+            var QuestionDetailDT = GetQuestionDetailAndItemDetail(guid);
+            //取得固定問題之使用者資料
+            var UserInfoString = this.Session["UserInfo"].ToString();
+            SaveUserInfo(guid, UserInfoString);
+            //取得使用者輸入問卷回覆內容
             string ansString = this.Session["ansString"].ToString();
             string[] ansStringArray = ansString.Split(',');
             for (int i = 0; i < QuestionDetailDT.Rows.Count; i++)
@@ -58,15 +71,18 @@ namespace SuverySystem
                 var QuestionDetailDR = QuestionDetailDT.Rows[i];
                 int DetailID = (int)QuestionDetailDR["DetailID"];
                 string AnswerString = ansStringArray[i];
-                SaveSuveryAnswer(guid, DetailID, AnswerString);
+                SaveSuveryAnswer(guid, DetailID, AnswerString, UserInfoString);
             }
             Response.Redirect("TryList.aspx");
-
         }
 
         #region Method區
-
-        public static void SaveSuveryAnswer(Guid guid,int DetailID, string AnswerString)
+        /// <summary>保存問卷回答</summary>
+        /// <param name="guid"></param>
+        /// <param name="DetailID"></param>
+        /// <param name="AnswerString"></param>
+        /// <param name="UserInfoString"></param>
+        public static void SaveSuveryAnswer(Guid guid, int DetailID, string AnswerString, string UserInfoString)
         {
             string connectionString = DBHelper.GetConnectionString();
             string dbCommandString =
@@ -74,16 +90,22 @@ namespace SuverySystem
                                             INSERT INTO [dbo].[AnswerDetail]
                                                        ([SuveryID]
                                                        ,[DetailID]
-                                                       ,[Answer])
+                                                       ,[Answer]
+                                                       ,[UserInfo]
+                                                       ,[CreateTime])
                                                  VALUES
                                                        (@Guid
                                                        ,@DetailID
-                                                       ,@Answer)                                          
+                                                       ,@Answer
+                                                       ,@UserInfo
+                                                       ,@CreateTime)                  
                                             ";
             List<SqlParameter> list = new List<SqlParameter>();
             list.Add(new SqlParameter("@Guid", guid));
             list.Add(new SqlParameter("@DetailID", DetailID));
             list.Add(new SqlParameter("@Answer", AnswerString));
+            list.Add(new SqlParameter("@UserInfo", UserInfoString));
+            list.Add(new SqlParameter("@CreateTime", DateTime.Now));
             try
             {
                 int effectRows = DBHelper.ModifyData(connectionString, dbCommandString, list);
@@ -93,7 +115,37 @@ namespace SuverySystem
                 Logger.WriteLog(ex);
             }
         }
+        /// <summary>保存固定問題之使用者資訊</summary>
+        /// <param name="guid"></param>
+        /// <param name="UserInfo"></param>
+        public static void SaveUserInfo(Guid guid,string UserInfo)
+        {
+            string connectionString = DBHelper.GetConnectionString();
+            string dbCommandString =
+                                            @"
+                                            INSERT INTO [dbo].[UserInfo]
+                                                       ([SuveryID]
+                                                       ,[UserInfo]
+                                                       ,[CreateTime])
+                                                 VALUES
+                                                       (@SuveryID
+                                                       ,@UserInfo
+                                                       ,@CreateTime)              
+                                            ";
+            List<SqlParameter> list = new List<SqlParameter>();
+            list.Add(new SqlParameter("@SuveryID", guid));
+            list.Add(new SqlParameter("@UserInfo", UserInfo));
+            list.Add(new SqlParameter("@CreateTime", DateTime.Now));
 
+            try
+            {
+                int effectRows = DBHelper.ModifyData(connectionString, dbCommandString, list);
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteLog(ex);
+            }
+        }
         /// <summary>取得問卷基本資料</summary>
         /// <param name="guid"></param>
         /// <returns></returns>
